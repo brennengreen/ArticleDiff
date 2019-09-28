ea6a68a58280fcaa10f9fe1060ef2082fbd467b0
Brennen Green <brennengreen@outlook.com>
2019-09-28 02:10:41 -0400

    init test

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const port = 80;
+
+app = express();
+app.listen(PORT);
+app.get('/', (req, res) => {
+    var hostname = process.env["HOSTNAME"].split("-");
+    res.send(hostname[hostname.length - 2]);
+});
+
+console.log(`Listening on ${PORT}`);
