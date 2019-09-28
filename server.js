7f71ec637fcb52c1d8d4d15023cca282947b7291
Brennen Green <brennengreen@outlook.com>
2019-09-28 07:55:51 -0400

    Index done

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-'use strict';
+//'use strict';
 const express = require('express');
 const PORT = process.env.PORT || 5000;
-const app = express();
+const path = require("path");
+var app = module.exports = express();
+
+// Define a templating engine
+app.set('view engine', 'pug')
+// Define a views folder
+app.set('views', path.join(__dirname, 'views'))
+
+// Serve static content
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(PORT);
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.status(200).render("index", {title: "News Artical Bias Analysis"})
 });
+
+
+
+// If there's nothing else to do then render 404
+
 
 console.log(`Listening on ${PORT}`);
